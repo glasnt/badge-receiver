33aa7c84e70c630dc081fad183d5b208b9407c78
Katie McLaughlin <katie@glasnt.com>
2020-07-13 16:04:55 +1000

    further cleanup

--- .gcloud/post-create.sh
+++ .gcloud/post-create.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 shopt -s expand_aliases
 
-stepdo() { echo "→ ${1}..." }
+stepdo() { 
+    echo "→ ${1}..." 
+}
+
+stepdone(){
+    statuscode=$?
+    msg="... done"
+    if [ $statuscode -ne 0 ]; then msg="❌  done, but non-zero return code ($statuscode)"; fi
+    echo $msg; echo " "
+}
 
 export SA_EMAIL=${K_SERVICE}@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
 export SERVICE_URL=$(gcloud run services describe $K_SERVICE --format "value(status.url)")
 
 stepdo "Assigning service account to service"
-gcloud run services update $K_SERVICE \
-    --service_account $SA_EMAIL
+gcloud run services update $K_SERVICE --service_account $SA_EMAIL
 stepdone
 
 echo "ℹ️  Use the following file for your cloud-build-notifier config.yaml: "
--- .gcloud/pre-build.sh
+++ .gcloud/pre-build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 shopt -s expand_aliases
 
-stepdo() { echo "→ ${1}..." }
-function quiet { $* > /dev/null }
+stepdo() { 
+    echo "→ ${1}..." 
+}
+
+function quiet { 
+    $* > /dev/null
+}
+
+stepdone(){
+    statuscode=$?
+    msg="... done"
+    if [ $statuscode -ne 0 ]; then msg="❌  done, but non-zero return code ($statuscode)"; fi
+    echo $msg
+    echo " "
+}
 
 export BUCKET=${GOOGLE_CLOUD_PROJECT}-media
 export SA_EMAIL=${K_SERVICE}@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
