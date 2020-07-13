4b31bcdc98ed4e082b81fd7493d37c6777a90fda
Katie McLaughlin <katie@glasnt.com>
2020-07-13 16:32:28 +1000

    Cleanup processes more

--- .gcloud/post-create.sh
+++ .gcloud/post-create.sh
@@ -16,12 +16,13 @@ export SA_EMAIL=${K_SERVICE}@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
 export SERVICE_URL=$(gcloud run services describe $K_SERVICE --format "value(status.url)")
 
 stepdo "Assigning service account to service"
-gcloud run services update $K_SERVICE --service_account $SA_EMAIL
+gcloud run services update $K_SERVICE \
+    --service-account $SA_EMAIL 
 stepdone
 
 echo "ℹ️  Use the following file for your cloud-build-notifier config.yaml: "
 echo ""
-sed -e "s/\${SERVICE_URL}/${SERVICE_URL}/" ../config.yaml
+sed -e "s|SERVICE_URL|${SERVICE_URL}|" ../config.yaml
 echo ""
 
 echo "Post-create configuration complete ✨"
