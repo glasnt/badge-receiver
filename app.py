01e308a23d79aad59b0eb45dc3149184ee775134
Katie McLaughlin <katie@glasnt.com>
2020-07-20 16:53:38 +1000

    Update debugging

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import io
-from flask import Flask, request, send_file
-import os
-from google.cloud import storage
-import google.auth
-from datetime import datetime
-import httpx
-
 import logging
+import os
+from datetime import datetime
+
+import dateparser
+import google.auth
+import httpx
+from babel.dates import format_timedelta
+from bs4 import BeautifulSoup as bs
+from flask import Flask, render_template, request, send_file
+from google.cloud import storage
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -15,14 +18,11 @@ client = storage.Client()
 _, project = google.auth.default()
 bucket = client.bucket(os.environ.get("BADGE_BUCKET", f"{project}-media"))
 
-@app.route("/")
-def hi():
-    return "HTTP 200 Beep Boop"
-
 
 # media bucket storage location (doesn't have to be nice)
 def service_badge_uri(service):
     return f"badge/{service}.svg"
+
 
 # nice URL
 @app.route("/badge/<name>.svg")
@@ -30,6 +30,16 @@ def service_badge(name):
     image = get_image(service_badge_uri(name))
 
     return send_file(image, mimetype=service_badge_mimetype())
+
+
+def relative_time(dstr):
+    if not dstr:
+        return "(no data)"
+    delta = dateparser.parse("now Z") - dateparser.parse(
+        dstr, settings={"TIMEZONE": "Z"}
+    )
+    return f"{format_timedelta(delta)} ago"
+
 
 def get_image(ident):
     blob = bucket.blob(ident)
@@ -39,14 +49,17 @@ def get_image(ident):
     image.seek(0)
     return image
 
+
 def service_badge_mimetype():
     return "image/svg+xml"
+
 
 def get_sub(data, key, default):
     if "substitutions" in data.keys():
         if key in data["substitutions"]:
             return data["substitutions"][key]
     return default
+
 
 @app.route("/receive", methods=["POST", "GET"])
 def receive():
@@ -61,28 +74,45 @@ def receive():
     # COULD use a step parser to see what the "gcloud run deploy X" value is
     # or query back from the trigger ID
     service = get_sub(data, "_SERVICE", "service")
-    label = service.replace("-","--")
+    label = service.replace("-", "--")
     commit = get_sub(data, "SHORT_SHA", "manual")
     status = "success" if data["status"] == "SUCCESS" else "critical"
 
-    badge_url = (f"https://img.shields.io/badge/{label}-{commit}-{status}"
-                   "?style=flat-square&logo=google-cloud&logoColor=white")
+    badge_url = (
+        f"https://img.shields.io/badge/{label}-{commit}-{status}"
+        "?style=flat-square&logo=google-cloud&logoColor=white"
+    )
     app.logger.info(f"Badge URL: {badge_url}")
     badge_blob = httpx.get(badge_url).content
 
     blob = storage.Blob(service_badge_uri(service), bucket=bucket)
-    blob.upload_from_string(badge_blob,
-            content_type=service_badge_mimetype())
+    blob.upload_from_string(badge_blob, content_type=service_badge_mimetype())
 
     app.logger.info(f"Badge uploaded to {blob.bucket.name} -- {blob.name}")
 
     return "Success", 201
 
+
 @app.route("/badge")
 def badge_list():
-    blobs =  list(bucket.list_blobs())
-    return "Known images: <li>" + "<li>".join([f"{x.name}" for x in blobs])
+    blobs = list(bucket.list_blobs())
+    data = []
+    for b in blobs:
+        svg = b.download_as_string()
+        text = ": ".join([x.text for x in bs(svg, "html.parser").find_all("text")])
+        data.append(
+            {
+                "name": b.name,
+                "updated": b.updated,
+                "relative": relative_time(str(b.updated)),
+                "data": text,
+            }
+        )
+    return render_template("badge.html", data=data)
 
+@app.route("/")
+def hi():
+    return "HTTP 200 Beep Boop"
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080, debug=True)
